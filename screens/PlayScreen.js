631f4bc3a1eef84c0c7f2a6a65b775d08ccf96bd
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2021-09-08 19:24:10 +0900

    Update PlayScreen.js

--- screens/PlayScreen.js
+++ screens/PlayScreen.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-    View,
-    Text,
-    StyleSheet,
-    Alert,
-    FlatList,
-    ScrollView,
-} from "react-native";
+import { View, Text, StyleSheet, Alert, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import DefaultStyles from "../constants/default-styles";
